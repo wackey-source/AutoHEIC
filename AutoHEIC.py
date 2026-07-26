a7fb7668b62ca0d7a8542e128ec6d813307cfa2e
wackey-source <wackey@gmail.com>
2026-07-26 23:18:19 +0200

    Working AutoHEIC core

---
+++ AutoHEIC.py
@@ -0,0 +1,60 @@
+import time
+
+from pillow_heif import register_heif_opener
+
+from modules.config import load_config
+from modules.log_manager import logger
+from modules.watcher import (
+    convert_existing_files,
+    create_observer,
+)
+
+
+def main():
+
+    register_heif_opener()
+
+    logger.info("========== Avvio AutoHEIC ==========")
+
+    cfg = load_config()
+
+    watch_folder = cfg["watch_folder"]
+    delete_original = cfg["delete_original"]
+    quality = cfg["quality"]
+    max_width = cfg["max_width"]
+    convert_existing = cfg["convert_existing"]
+
+    if convert_existing:
+        convert_existing_files(
+            watch_folder,
+            quality,
+            delete_original,
+            max_width,
+        )
+
+    observer = create_observer(
+        watch_folder,
+        quality,
+        delete_original,
+        max_width,
+    )
+
+    observer.start()
+
+    logger.info(f"Monitoraggio cartella: {watch_folder}")
+
+    try:
+        while True:
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        logger.info("Arresto richiesto dall'utente.")
+
+    finally:
+        observer.stop()
+        observer.join()
+        logger.info("AutoHEIC terminato.")
+
+
+if __name__ == "__main__":
+    main()
