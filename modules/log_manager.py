79f1f6259c9fff1fcd951c1ca6f0f95e608e833e
wackey-source <wackey@gmail.com>
2026-07-26 22:52:48 +0200

    Create logging module

--- modules/log_manager.py
+++ modules/log_manager.py
@@ -8,15 +8,16 @@ LOG_FILE = os.path.join(LOG_DIR, "autoheic.log")
 os.makedirs(LOG_DIR, exist_ok=True)
 
 logger = logging.getLogger("AutoHEIC")
-logger.setLevel(logging.INFO)
 
 if not logger.handlers:
+
+    logger.setLevel(logging.INFO)
 
     handler = RotatingFileHandler(
         LOG_FILE,
         maxBytes=1024 * 1024,
         backupCount=5,
-        encoding="utf-8"
+        encoding="utf-8",
     )
 
     formatter = logging.Formatter(
